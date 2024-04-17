f6024992ca2c0bbbc135f10d6b5faaa2f3b4c993
Piotr Dabrowski <kedan@kedan.pl>
2024-04-17 11:22:12 +0200

    Documentation update.

--- doc/html/search/all_5.js
+++ doc/html/search/all_5.js
@@ -4,5 +4,6 @@ var searchData=
   ['find_1',['Find',['../d3/d40/classn2d_1_1JSONLoader.html#aff208a9b69cc12fe8310de1f6a764a7a',1,'n2d::JSONLoader']]],
   ['flush_2',['Flush',['../d4/db7/classn2d_1_1Logger.html#a9cb660368605951c1ea0978604cc48e4',1,'n2d::Logger']]],
   ['frame_3',['Frame',['../df/dbf/classn2d_1_1Frame.html',1,'n2d::Frame'],['../df/dbf/classn2d_1_1Frame.html#ac42d51816ff365ce9fdefdb5cfd54caa',1,'n2d::Frame::Frame()']]],
-  ['frameanimation_4',['FrameAnimation',['../d3/d9a/classn2d_1_1FrameAnimation.html',1,'n2d']]]
+  ['frameanimation_4',['FrameAnimation',['../d3/d9a/classn2d_1_1FrameAnimation.html',1,'n2d']]],
+  ['free_5',['Free',['../da/d95/classn2d_1_1Resources.html#ad10c2a54cb28aaf76082169a7eaf42b6',1,'n2d::Resources']]]
 ];
